595eb9636f36770499e09d27d085365560274ede
mralimov <mralimov89@gmail.com>
2020-12-09 10:05:34 -0500

    Update index.js

--- index.js
+++ index.js
@@ -4,4 +4,21 @@ let randomNumber = Math.floor(Math.random() * 20) + 1;
 let score = 20;
 let highScore = 0;
 
-const checkNumbers = function (num) {};
+// const message = document.querySelector('.message');
+const btnCheck = document
+  .querySelector('.check')
+  .addEventListener('click', function () {
+    const inputNum = Number(document.querySelector('.guess').value);
+    console.log(inputNum);
+    if (!inputNum) {
+      document.querySelector('.message').textContent = '⛔️ No number!';
+    } else if (inputNum > randomNumber) {
+      document.querySelector('.message').textContent = '📈 Too high!';
+    }
+  });
+
+//🎉 Correct Number!
+//📈 Too high!
+//💥 You lost the game!
+//📉 Too low!
+//⛔️ No number!
